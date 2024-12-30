d68dd3bf9378ab081c435aa72c750727b9e09e4c
Vasanth Gopal <vasanth.gopal0@loblaw.ca>
2024-12-30 15:43:47 -0500

    day 1 and day 2

---
+++ src/main/kotlin/com/advent/of/code/_2024/Application.kt
@@ -0,0 +1,11 @@
+package com.advent.of.code._2024
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
